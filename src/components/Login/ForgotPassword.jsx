5cf25562c3a8c464cefbd88aed2d08e3bfe5f7d8
Sajid Chowdhury <msrchk@gmail.com>
2022-04-12 02:18:24 +0600

    lofin functionality implemented

--- src/components/Login/ForgotPassword.jsx
+++ src/components/Login/ForgotPassword.jsx
@@ -1,30 +1,67 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import NavBar from "../Common/NavBar/NavBar.jsx";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError({});
+    try {
+      if (email) {
+        const re = /\S+@\S+\.\S+/;
+        const isValid = re.test(email);
+        if (isValid) {
+          const response = await axios.post("http://localhost:5000/auth/sendForgotPasswordEmail", { email });
+          console.log(response);
+          if (response.status === 200) {
+            navigate("/resetPassword");
+            setError({});
+            setIsLoading(false);
+          }
+        } else {
+          setError({ msg: "Email is not valid" });
+          setIsLoading(false);
+        }
+      } else {
+        setError({ msg: "Email is required" });
+        setIsLoading(false);
+      }
+    } catch (err) {
+      setError({ msg: "Something wrong......." });
+      setIsLoading(false);
+    }
+  }
   return (
     <div>
       <NavBar />
       <div className="d-flex justify-content-center mt-5">
         <div>
+          {
+            error.msg && <div className="alert alert-danger">{error.msg}</div>
+          }
           <div>
             <h2>Demander un nouveau mot de passe</h2>
             <p className="m-0">
               Entrez votre adresse e-mail. Nous vous enverrons un lien pour
               créer un nouveau mot de passe.
             </p>
-
             <div className="my-5">
               <label htmlFor="">
                 Adresse e-mail <span className="text-danger">*</span>
               </label>
               <br />
-              <input className="forget_input" type="email" name="" id="" />
+              <input className="forget_input" type="email" name="" id="" onChange={e => { setEmail(e.target.value); setError({}) }} />
             </div>
             <Link to="/resetPassword">
-              <button className="btn btn-primary rounded-pill px-3 py-2">
-                Envoyer
+              <button className= {`btn btn-primary rounded-pill px-3 py-2 ${isLoading ? "w-25" : ""}`} onClick={handleResetPassword}>
+                {isLoading ? <div className="spinner-border text-light" role="status"></div> : "Envoyer"}
               </button>
             </Link>
           </div>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,71 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { FcGoogle } from 'react-icons/fc';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
 import NavBar from "../Common/NavBar/NavBar";
 const Login = () => {
+  const [loginInfo, setLoginInfo] = useState({})
+  const [showPass, setShowPass] = useState(false);
+  const [error, setError] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleShowPass = () => {
+    setShowPass(!showPass);
+  }
+  const handleChange = (e) => {
+    const newLoginInfo = {};
+    if (e.target.name === "email") {
+      const re = /\S+@\S+\.\S+/;
+      const isValid = re.test(e.target.value);
+      if (isValid) {
+        newLoginInfo.email = e.target.value;
+        setError({});
+      } else {
+        setError({ email: "Email is not valid" });
+      }
+    } else {
+      const re = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+      const isValid = re.test(e.target.value);
+      newLoginInfo.password = e.target.value;
+      setError({});
+      // if(isValid){
+      //   newLoginInfo.password = e.target.value;
+      //   setError({});
+      // } else{
+      //   // setError({password: "Password must contain at least 8 characters, one letter and one number"});
+      // }
+    }
+
+    setLoginInfo({ ...loginInfo, ...newLoginInfo });
+
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (loginInfo.email && loginInfo.password) {
+        console.log(loginInfo);
+        setIsLoading(true);
+        const response = await axios.post('https://ancient-gorge-88070.herokuapp.com/auth/signIn', loginInfo);
+        if (response?.data) {
+          Cookies.set('accessToken', response?.data?.data?.accessToken);
+          Cookies.set('refreshToken', response?.data?.data?.refreshToken);
+          setIsLoading(false);
+          setError({});
+          navigate('/');
+          console.log(response?.data);
+        }
+      } else {
+        setError({ msg: "Please fill all the fields" });
+      }
+    } catch (err) {
+      console.log(err);
+      setError({ msg: "Invalid email or password" });
+      setIsLoading(false);
+    }
+  }
   return (
     <div className="login">
       <NavBar />
@@ -10,6 +73,9 @@ const Login = () => {
         <h1 style={{ fontSize: "50px" }} className="mb-5">
           Connexion
         </h1>
+        {
+          error.msg && <div className="alert alert-danger">{error.msg}</div>
+        }
         <form action="">
           <p>
             E-mail <span className="text-danger">*</span>
@@ -17,10 +83,14 @@ const Login = () => {
           <input
             type="email"
             className="form-control py-2 w-75 w-sm-100"
-            name=""
+            name="email"
+            onChange={handleChange}
             id=""
           />{" "}
           <br />
+          {
+            error.email && <p className="text-danger">{error.email}</p>
+          }
           <p>
             Mot de passe <span className="text-danger mt-4">*</span>
           </p>
@@ -29,32 +99,40 @@ const Login = () => {
             className="login_pass w-100 d-flex  align-items-center"
           >
             <input
-              type="password"
+              type={showPass ? "text" : "password"}
               className="form-control py-2 w-75 w-sm-100 "
-              name=""
+              onChange={handleChange}
+              name="password"
               id=""
             />
 
             <div className="show_pass d-flex align-items-center justify-content-center gap-2">
-              <input type="checkbox" name="" id="" />
+              <input type="checkbox" onChange={handleShowPass} name="" id="" />
               <span>Afficher</span>
             </div>
           </label>
+          {
+            error.password && <p className="text-danger">{error.password}</p>
+          }
           <p className="pt-5 ">
             <Link className="text-primary" to="/forgetPassword">
               Mot de passe oublié ?
             </Link>
           </p>
-          <input type="submit" value="Connexion" className="main_button" />
+          <button type="submit" className={isLoading ? "main_button w-25" : "main_button"} onClick={handleSubmit}>
+            {
+              isLoading ? <div className="spinner-border text-light" role="status"></div> : "Connexion"
+            }
+          </button>
         </form>
         <p>Ou continuez simplement avec</p>
         <button className="socialButton">
-          
-            <span className="google_icon">
-              <FcGoogle />
-            </span>
-            Google / Email
-          
+
+          <span className="google_icon">
+            <FcGoogle />
+          </span>
+          Google / Email
+
         </button>
         <p>Your personal data will not be shared with our site.</p>
       </div>
