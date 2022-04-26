6bf4a1311ede2e2f7a6eac0420f45ec9d2a17427
HafezFaruk <hmomarfaruk1454@gmail.com>
2022-04-27 04:31:29 +0600

    My Projects Page added

---
+++ src/data/MyProjectsPage.js
@@ -0,0 +1,24 @@
+const MyProjectsPage = [
+  {
+    title: "Demolition: Entire facade; Apartment; 12 m²",
+    name: "You don't have an answer yet",
+    
+  },
+  {
+    title: "Demolition",
+    name: "You don't have an answer yet",
+    
+  },
+  {
+    title: "Demolition: Entire facade; ",
+    name: "You don't have an answer yet",
+    
+  },
+  {
+    title: "Demolition: Entire ",
+    name: "You don't have an answer yet",
+    
+  },
+];
+
+export default MyProjectsPage;
